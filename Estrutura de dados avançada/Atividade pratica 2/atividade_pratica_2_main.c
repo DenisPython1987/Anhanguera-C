e27d8ae2652ef129b8c14a96d70f41f179fb1de5
DenisPython1987 <denisandervivan2@gmail.com>
2026-08-11 22:47:14 -0300

    Término do trabalho

--- Estrutura de dados avançada/Atividade pratica 2/atividade_pratica_2_main.c
+++ Estrutura de dados avançada/Atividade pratica 2/atividade_pratica_2_main.c
@@ -20,18 +20,26 @@ int main(){
     inicio_2 = clock();
     insertionSort(numeros, 1000);
     fim_2 = clock();
+    double tempo_2 = (double)(fim_2 - inicio_2) / CLOCKS_PER_SEC;
+    printf("Tempo de execução do insertion sort: %.6f segundos\n", tempo_2);
 
     inicio_3 = clock();
     mergeSort(numeros, 0, 999);
     fim_3 = clock();
+    double tempo_3 = (double)(fim_3 - inicio_3) / CLOCKS_PER_SEC;
+    printf("Tempo de execução do merge sort: %.6f segundos\n", tempo_3);
 
     inicio_4 = clock();
     quickSort(numeros, 0, 999);
     fim_4 = clock();
+    double tempo_4 = (double)(fim_4 - inicio_4) / CLOCKS_PER_SEC;
+    printf("Tempo de execução do quick sort: %.6f segundos\n", tempo_4);
 
     inicio_5 = clock();
     heapSort(numeros, 1000);
     fim_5 = clock();
+    double tempo_5 = (double)(fim_5 - inicio_5) / CLOCKS_PER_SEC;
+    printf("Tempo de execução do heap sort: %.6f segundos\n", tempo_5);
 
     return 0;
 }
