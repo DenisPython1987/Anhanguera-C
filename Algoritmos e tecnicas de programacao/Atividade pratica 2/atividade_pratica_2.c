bf7880bba09f7510596a9723c3f224b5caf04449
DenisPython1987 <denisandervivan2@gmail.com>
2026-09-23 22:03:48 -0300

    Conclusão da atividade prática

--- Algoritmos e tecnicas de programacao/Atividade pratica 2/atividade_pratica_2.c
+++ Algoritmos e tecnicas de programacao/Atividade pratica 2/atividade_pratica_2.c
@@ -7,19 +7,43 @@ int main(){
     float valor_final;
     int anos;
 
-    int vetor[3] = {}
+    int *ponteiro_vetor;
 
+    int vetor[3] = {1, 2, 3};
+
+    int matriz[2][2] = {{1, 2}, {3, 4}};
+
+    ponteiro_vetor = &vetor[0];
+    printf("------------------------------------------------------------\n");
     printf("Digite o valor inicial do investimento: ");
     scanf("%f", &valor_inicial);
 
+    printf("------------------------------------------------------------\n");
     printf("Digite o tempo em anos do investimento: ");
     scanf("%d", &anos);
 
     valor_final = valor_inicial * (1 + (TAXA_JUROS * anos));
 
-    printf("Valor final do investimento: %f", valor_final);
+    printf("Valor final do investimento: %.2f\n", valor_final);
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("------------------------------------------------------------\n");
+        printf("Valor do vetor: %d\n", vetor[i]);
+        printf("Endereço de memória: %p\n", (void*)&vetor[0]);
+    }
+    
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("------------------------------------------------------------\n");
+            printf("Valor na posição [%d][%d] = %d\n", i, j, matriz[i][j]);
+            printf("Endereço de memória da posição [%d][%d] = %p\n", i, j, (void*)&matriz[i][j]);
+        }
+        
+    }
+    printf("------------------------------------------------------------\n");
+
 
 }
-
-
-
